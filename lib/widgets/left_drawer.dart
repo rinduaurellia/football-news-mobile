0f994bbe65b267d25c9dcf6ed7da6cf6366079c2
rinduaurellia <rindu.aurellia@ui.ac.id>
2025-11-12 16:44:03 +0700

    tutorial 8 selesai

--- lib/widgets/left_drawer.dart
+++ lib/widgets/left_drawer.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:football_news/screens/menu.dart';
 import 'package:football_news/screens/newslist_form.dart';
+import 'package:football_news/screens/news_entry_list.dart';
+
 
 class LeftDrawer extends StatelessWidget {
   const LeftDrawer({super.key});
@@ -67,6 +69,17 @@ class LeftDrawer extends StatelessWidget {
               setelah halaman NewsFormPage sudah dibuat.
               */
             },
+          ),
+          // Add this ListTile in your drawer
+          ListTile(
+              leading: const Icon(Icons.add_reaction_rounded),
+              title: const Text('News List'),
+              onTap: () {
+                  Navigator.push(
+                      context,
+                      MaterialPageRoute(builder: (context) => const NewsEntryListPage()),
+                  );
+              },
           ),
         ],
       ),
--- lib/widgets/news_card.dart
+++ lib/widgets/news_card.dart
@@ -1,16 +1,20 @@
 import 'package:flutter/material.dart';
 import 'package:football_news/screens/newslist_form.dart';
 import 'package:football_news/screens/menu.dart';
+import 'package:football_news/screens/news_entry_list.dart';
+import 'package:football_news/screens/login.dart';
+import 'package:pbp_django_auth/pbp_django_auth.dart';
+import 'package:provider/provider.dart';
 
 class ItemCard extends StatelessWidget {
   // Menampilkan kartu dengan ikon dan nama.
+  final ItemHomepage item;
 
-  final ItemHomepage item; 
-
-  const ItemCard(this.item, {super.key}); 
+  const ItemCard(this.item, {super.key});
 
   @override
   Widget build(BuildContext context) {
+  final request = context.watch<CookieRequest>();
     return Material(
       // Menentukan warna latar belakang dari tema aplikasi.
       color: Theme.of(context).colorScheme.secondary,
@@ -19,21 +23,51 @@ class ItemCard extends StatelessWidget {
 
       child: InkWell(
         // Aksi ketika kartu ditekan.
-        onTap: () {
-        // Memunculkan SnackBar ketika diklik
-        ScaffoldMessenger.of(context)
-          ..hideCurrentSnackBar()
-          ..showSnackBar(SnackBar(
-              content: Text("Kamu telah menekan tombol ${item.name}!")));
+        onTap: () async{
+          // Memunculkan SnackBar ketika diklik
+          ScaffoldMessenger.of(context)
+            ..hideCurrentSnackBar()
+            ..showSnackBar(SnackBar(
+                content: Text("Kamu telah menekan tombol ${item.name}!")));
 
-        // Navigate ke route yang sesuai (tergantung jenis tombol)
-        if (item.name == "Add News") {
-         Navigator.push(
+          // Navigate ke route yang sesuai (tergantung jenis tombol)
+          if (item.name == "Add News") {
+            Navigator.push(
               context,
-              MaterialPageRoute(builder: (context) => NewsFormPage()),
+              MaterialPageRoute(builder: (context) => const NewsFormPage()),
             );
-          }
-      },
+          } else if (item.name == "See Football News") {
+            Navigator.push(
+              context,
+              MaterialPageRoute(
+                  builder: (context) => const NewsEntryListPage()),
+            );
+          } else if (item.name == "Logout") {
+                // To connect Android emulator with Django on localhost, use URL http://10.0.2.2/
+                // If you using chrome,  use URL http://localhost:8000
+                final response = await request.logout(
+                    "http://localhost:8000/auth/logout/");
+                String message = response["message"];
+                if (context.mounted) {
+                    if (response['status']) {
+                        String uname = response["username"];
+                        ScaffoldMessenger.of(context).showSnackBar(SnackBar(
+                            content: Text("$message See you again, $uname."),
+                        ));
+                        Navigator.pushReplacement(
+                            context,
+                            MaterialPageRoute(builder: (context) => const LoginPage()),
+                        );
+                    } else {
+                        ScaffoldMessenger.of(context).showSnackBar(
+                            SnackBar(
+                                content: Text(message),
+                            ),
+                        );
+                    }
+                }
+            }
+        },
         // Container untuk menyimpan Icon dan Text
         child: Container(
           padding: const EdgeInsets.all(8),
@@ -61,4 +95,3 @@ class ItemCard extends StatelessWidget {
     );
   }
 }
-
